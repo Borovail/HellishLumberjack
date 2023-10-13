359a83d75eafb8c7f8b1890b04c5e78e6980887e
Borovail <skorikdaniil17@gmail.com>
2023-10-13 16:02:47 +0300

    MonsterSpawner
    test Monster Behavior , when touch the player dies,  + chasing the player although barriers.
    Objects pool  spawn a monster in area that can be configured

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -1,27 +1,35 @@
+﻿using Assets.Scripts.Interfaces;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
+using UnityEngine.UIElements;
 
-public class Monster : BaseMonster
+public class Monster : BaseMonster,IPoolable
 {
 
-
-    [SerializeField] private Transform player;
+    private Transform player;
     [SerializeField] private LayerMask playerLayer;
 
     private float Speed = 2;
     private float detectionDistance = 1f;
-    private bool IsAttacking = false;
+    public bool IsAttacking = false;
     /// can be changed probably
-    private float distanceForAttack = 1.3f;
+    private float distanceForAttack = 2f;
     private Vector2 capsuleSize = new Vector2(0.5f, 0.5f);
+
+    public event EventHandler<IPoolable> OnObjectDeactivation;
 
     private void FixedUpdate()
     {
         ChasePlayer();
     }
 
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
     /// <summary>
     /// /perhaps this will require corrections due to twitching when it hits the player, but after the finished textures
     /// </summary>
@@ -33,51 +41,67 @@ public class Monster : BaseMonster
         Vector3 direction = (player.position - transform.position).normalized;
         float distanceToPlayer = Vector3.Distance(transform.position, player.position);
 
-        if(distanceToPlayer < distanceForAttack)
+ 
+
+        if (distanceToPlayer < distanceForAttack)
         {
-            if (!IsAttacking) 
-           StartCoroutine( Attack());
+            if (!IsAttacking)
+                ObjectDeactivation();
+            //    StartCoroutine(Attack());
             return;
         }
 
         // Check for some barrier in front of monster with shape of a capsule
-        var capsule = Physics2D.Raycast(transform.position, direction, detectionDistance, playerLayer);/*transform.position, capsuleSize, CapsuleDirection2D.Vertical, 0, direction, detectionDistance, playerLayer);*/
-        Debug.DrawRay(transform.position, direction, Color.red,5f);
+        var capsule = Physics2D.Raycast(transform.position , direction, detectionDistance, playerLayer);/*transform.position, capsuleSize, CapsuleDirection2D.Vertical, 0, direction, detectionDistance, playerLayer);*/
+        Debug.DrawRay(transform.position, direction, Color.red, 5f);
 
 
 
         /////if something is in front  and it is not a player ,   later need  to change PlayerController as component
-        if (capsule.collider != null && capsule.collider.transform!=player)
+        if (capsule.collider != null && capsule.collider.transform != player)
         {
-            // Compute perpendicular directions
-            Vector2 leftPerpendicular = new Vector2(-direction.y, direction.x);
-            Vector2 rightPerpendicular = new Vector2(direction.y, -direction.x);
 
-            // Look for vertical direction to get around
-            var verticalRay = Physics2D.Raycast(transform.position, leftPerpendicular, detectionDistance, playerLayer);
-            Debug.DrawRay(transform.position, leftPerpendicular * detectionDistance, Color.red);
+            /*/// <summary>
+            /// The leftDir and topDir variables are vectors perpendicular to the main direction of movement. These perpendicular vectors are calculated by rotating the original vector 90 degrees around the origin.
+            In 2D space, vector  (x, y) can be rotated 90 degrees, resulting in a vector (−y,x) or (y,−x).
+            leftDir = new Vector2(-direction.y, direction.x) rotates direction 90 degrees counterclockwise.
+            topDir = new Vector2(direction.y, -direction.x) rotates direction 90 degrees clockwise.
+            This allows you to check "lateral" directions relative to the main direction of travel.
+            /// </summary>*/
+            var leftDir = new Vector2(-direction.y,direction.x);
+            var topDir = new Vector2(direction.y, -direction.x);
+         
 
-            /////if there is nothing vertically and it is not a player
-            if (verticalRay.collider == null )
+            ////throw ray to the left for checking if it's direction is clear;
+            var leftRay = Physics2D.Raycast(transform.position, leftDir, detectionDistance, playerLayer);
+            Debug.DrawRay(transform.position, leftDir, Color.black, 5f);
+
+            if(leftRay.collider == null)
             {
-                direction = new Vector3(direction.x, -direction.y);
+                direction = leftDir;
+
             }
             else
             {
-                // Look for horizontal direction to get around
-                var horizontalRay = Physics2D.Raycast(transform.position, rightPerpendicular, detectionDistance, playerLayer);
-                Debug.DrawRay(transform.position, rightPerpendicular * detectionDistance, Color.red);
+                ///throw ray to the top for checking if it's direction is clear;
+                var topRay = Physics2D.Raycast(transform.position, topDir, detectionDistance, playerLayer);
+                Debug.DrawRay(transform.position, topDir, Color.blue, 5f);
 
-                /////if there is nothing horizontally and it is not a player
-                if (horizontalRay.collider == null )
+
+                if(topRay.collider == null)
                 {
-                    direction = new Vector3(-direction.x, direction.y);
+                    direction = topDir;
                 }
+                
             }
-        }
 
+        }
         transform.position += direction * Speed * Time.deltaTime;
+
     }
+
+
+
 
 
     public override IEnumerator Attack()
@@ -90,7 +114,16 @@ public class Monster : BaseMonster
         IsAttacking = false;
     }
 
-    //public override void Die()
+    public GameObject GetGameObject() => gameObject;
+
+    public void ObjectDeactivation()
+    {
+        OnObjectDeactivation?.Invoke(this, this);
+
+        IsAttacking = true;
+    }
+
+    //public override void Die(),
     //{
     //    throw new System.NotImplementedException();
     //}
---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,68 @@
+﻿using Assets.Scripts.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using IPoolable = Assets.Scripts.Interfaces.IPoolable;
+
+namespace Assets.Scripts
+{
+    public class ObjectPool : MonoBehaviour
+    {
+        [SerializeField] private GameObject[] objects;
+        [SerializeField] private int initialSize =5;
+        private Queue<IPoolable> objectPool;
+
+
+
+        private void Start()
+        {
+            objectPool = new Queue<IPoolable>();
+
+            for (int i = 0; i < initialSize; i++)
+            {
+                foreach (var  item in objects)
+                {
+                    var obj = Instantiate(item);
+
+                    if (!obj.TryGetComponent(out IPoolable poolAbleObj)) continue;
+
+                    poolAbleObj.OnObjectDeactivation += PoolAbleObj_OnObjectDeactivation;
+
+                    SetGameObjectToPool(poolAbleObj);
+                }
+            }
+        }
+
+        private void PoolAbleObj_OnObjectDeactivation(object sender, IPoolable e)
+        {
+                    SetGameObjectToPool(e);
+        }
+
+
+
+        public void GetObjectOfPool(Vector2 position)
+        {
+            if (objectPool.Count == 0) return;
+
+            var obj = objectPool.Dequeue().GetGameObject();
+
+            obj.SetActive(true);
+
+            obj.GetComponent<Monster>().IsAttacking = false;
+
+            obj.transform.position = position;
+        }
+
+        public void SetGameObjectToPool(IPoolable gameObject)
+        {
+            gameObject.GetGameObject().SetActive(false);
+
+            objectPool.Enqueue(gameObject);
+        }
+
+
+    }
+}
